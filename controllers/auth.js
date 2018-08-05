ff3cd5c33f9216fbe1b8f8290be281a8e2521292
Gabriel Rufino <contato@gabrielrufino.com>
2018-08-05 17:25:26 -0300

    Auth with jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,26 @@
-const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 
 const controller = {}
 const secret = process.env.SECRET
+
+controller.get = async (req, res) => {
+  const token = req.headers.authorization
+
+  if (!token) {
+    return res.status(401).json({ authenticated: 'false', error: 'No token provided' })
+  }
+
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ authenticated: 'false', error: err })
+    }
+
+    return res.status(200).json({ authenticated: 'true', user: decoded.id })
+  })
+}
 
 controller.post = async (req, res) => {
   const { username, password } = req.body
@@ -19,8 +35,7 @@ controller.post = async (req, res) => {
     return res.status(400).json({ error: 'Invalid password' })
   }
 
-  const token = jwt.sign({ id: user.id }, secret, { expiresIn: 10 })
-
+  const token = jwt.sign({ id: user.id }, secret, { expiresIn: 20 })
   return res.status(200).json({ user, token })
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/auth')
 
+router.get('/', controller.get)
 router.post('/', controller.post)
 
 module.exports = router
