c3412e02005af9880a9ce0d2cf8c6d4561535c76
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-05 16:21:40 -0300

    Removing environment variables default values from the code

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const db       = mongoose.connection
 
-const DB_USER = process.env.DB_USER || ''
-const DB_PASS = process.env.DB_PASS || ''
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_PORT = process.env.DB_PORT || '27017'
-const DB_NAME = process.env.DB_NAME || 'db_ubox'
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_NAME = process.env.DB_NAME
 
 const databaseUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 mongoose.connect(databaseUrl, { useNewUrlParser: true })
