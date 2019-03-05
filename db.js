99a2aa45426c786b46dd749152ae745917ff710f
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-05 16:01:42 -0300

    Improvement in the database connection

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 const db = mongoose.connection
 
-const database = process.env.DATABASE || 'mongodb://localhost/ubox'
-mongoose.connect(database, { useNewUrlParser: true })
+const DB_USER = process.env.DB_USER || ''
+const DB_PASS = process.env.DB_PASS || ''
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_PORT = process.env.DB_PORT || '27017'
+const DB_NAME = process.env.DB_NAME || 'db_ubox'
+
+const databaseUrl = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+mongoose.connect(databaseUrl, { useNewUrlParser: true })
 
 db.on('error', console.error)
 db.once('open', () => {
