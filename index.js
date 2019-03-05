d37c3d2a06ab58f6d9fd0b1e78ba71011eaf7bdc
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-05 16:16:50 -0300

    Crashing the application if doesn't exist a .env file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
+const result = dotenv.config()
+
+if (result.error) {
+  throw new Error(result.error)
+}
+
 require('./db')
 require('./app')
