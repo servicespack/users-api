2653ff732cbb36e6adcd5fa04909277bde5311c5
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-06 17:26:17 -0300

    Validation of the token prefix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const TOKEN_SECRET = process.env.TOKEN_SECRET
+const prefixes     = ['Bearer']
 
 const auth = (req, res, next) => {
   const authorization = req.headers.authorization
@@ -12,7 +13,15 @@ const auth = (req, res, next) => {
     })
   }
 
-  jwt.verify(authorization, TOKEN_SECRET, (err, decoded) => {
+  const [prefix, token] = authorization.split(' ')
+
+  if (!prefixes.includes(prefix)) {
+    return res.status(401).json({
+      error: 'Invalid prefix'
+    })
+  }
+
+  jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({ authenticated: 'false', error: err })
     }
