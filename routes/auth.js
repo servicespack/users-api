3c4f6a70fee0f04dde0021e717255b02af9d7c81
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-06 16:28:37 -0300

    Removing route to get /auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const express = require('express')
 const controllers = require('../controllers/auth')
 const router      = express.Router()
 
-router.get('/', controllers.get)
 router.post('/', controllers.post)
 
 module.exports = router
