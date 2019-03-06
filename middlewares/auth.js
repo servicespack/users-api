fe8228be611c3858b6124230320e0e1d6c8dcfc2
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-06 15:57:26 -0300

    Initia auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,22 @@
+const jwt = require('jsonwebtoken')
+
 const auth = (req, res, next) => {
-  console.log('middleware auth')
-  next()
+  const authorization = req.headers.authorization
+
+  if (!authorization) {
+    return res.status(401).json({
+      authenticated: 'false',
+      error: 'No token provided'
+    })
+  }
+
+  jwt.verify(authorization, process.env.SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ authenticated: 'false', error: err })
+    }
+
+    return next()
+  })
 }
 
 module.exports = auth
