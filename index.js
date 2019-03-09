4e03874caf50cb7ee2ba23d41030f4959783ef4a
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-09 11:45:10 -0300

    Loading .env.test for test environment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv')
-const result = dotenv.config()
+const result = dotenv.config({
+  path: process.env.APP_ENV === 'test' ? '.env.test' : '.env'
+})
 
 if (result.error) {
   throw new Error(result.error)
