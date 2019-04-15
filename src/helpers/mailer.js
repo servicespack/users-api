aeee945c5bc2fc31e385a4049409139ecc008fcd
Gabriel Rufino <contato@gabrielrufino.com>
2019-04-15 09:56:44 -0300

    initial helper mailer

---
+++ src/helpers/mailer.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer')
+
+const MAILER_HOST = process.env.MAILER_HOST
+const MAILER_PORT = process.env.MAILER_PORT
+const MAILER_SECURE = process.env.MAILER_SECURE
+const MAILER_USER = process.env.MAILER_USER
+const MAILER_PASS = process.env.MAILER_PASS
+
+const transporter = nodemailer.createTransport({
+  host: MAILER_HOST,
+  port: MAILER_PORT,
+  secure: MAILER_SECURE,
+  auth: {
+    user: MAILER_USER,
+    pass: MAILER_PASS
+  }
+})
+
+const sendMail = async (to, subject, html) => {
+  await transporter.sendMail({
+    from: 'example@example.com',
+    to: to,
+    subject: subject,
+    html: html
+  })
+}
+
+module.exports = {
+  sendMail
+}
