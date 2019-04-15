9fc1ea413fa03029cdead5fe7efea1d89ac586db
Gabriel Rufino <contato@gabrielrufino.com>
2019-04-15 19:48:39 -0300

    Upgrade in the mailer helper

--- src/helpers/mailer.js
+++ src/helpers/mailer.js
@@ -1,17 +1,17 @@
 const nodemailer = require('nodemailer')
 
-const MAILER_HOST = process.env.MAILER_HOST
-const MAILER_PORT = process.env.MAILER_PORT
-const MAILER_SECURE = process.env.MAILER_SECURE
-const MAILER_USER = process.env.MAILER_USER
-const MAILER_PASS = process.env.MAILER_PASS
+const MAILER_HOST   = process.env.MAILER_HOST
+const MAILER_PORT   = process.env.MAILER_PORT
+const MAILER_SECURE = process.env.MAILER_SECURE === 'true' ? true : false
+const MAILER_USER   = process.env.MAILER_USER
+const MAILER_PASS   = process.env.MAILER_PASS
 
 const transporter = nodemailer.createTransport({
   host: MAILER_HOST,
   port: MAILER_PORT,
   secure: MAILER_SECURE,
   tls: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   },
   auth: {
     user: MAILER_USER,
