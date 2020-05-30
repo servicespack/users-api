bfa6afbaf374ba41095bc615aee0a6a0cf30f517
Gabriel Rufino <contato@gabrielrufino.com>
2020-05-30 01:57:52 -0300

    Encrypt passwords on script database:populate

--- scripts/database:populate.js
+++ scripts/database:populate.js
@@ -1,14 +1,17 @@
 const { db } = require('../start/')
 
 if (process.env.APP_ENV === 'development') {
+  const bcrypt = require('bcryptjs')
   const faker = require('faker')
   const mongoose = require('mongoose')
 
-  const users = Array(100).fill(null).map(() => ({
+  const salt = bcrypt.genSaltSync(10)
+
+  const users = Array(1000).fill(null).map(() => ({
     name: faker.name.findName(),
     email: faker.internet.email(),
     username: faker.internet.userName(),
-    password: faker.internet.password()
+    password: bcrypt.hashSync(faker.internet.password(), salt)
   }))
 
   const User = mongoose.model('User')
