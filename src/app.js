1ae8ea18f74530b8eac505b3d8d1111795396015
Gabriel Rufino <contato@gabrielrufino.com>
2020-06-02 21:20:12 -0300

    Using helmet for security

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
+const helmet = require('helmet')
 
 const app = express()
 
-app.disable('x-powered-by')
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(helmet())
 
 app.use('/api', require('./routes/index'))
 app.use('/api/users', require('./routes/users'))
