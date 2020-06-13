13a9b90909bd1a55b84dfe4328392b221df37386
Gabriel Rufino <contato@gabrielrufino.com>
2020-06-13 16:15:20 -0300

    Now, only the own user can delete or update yourself

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,15 +2,15 @@
 
 const express = require('express')
 
-const authMiddleware = require('../middlewares/auth')
+const auth = require('../middlewares/auth')
 const validators = require('../middlewares/validators/users')
 const controllers = require('../controllers/users')
 const router = express.Router()
 
 router.post('/', [validators.create], controllers.create)
-router.get('/', [authMiddleware, validators.list], controllers.list)
-router.get('/:id', [authMiddleware], controllers.show)
-router.patch('/:id', [authMiddleware, validators.update], controllers.update)
-router.delete('/:id', [authMiddleware], controllers.delete)
+router.get('/', [auth(), validators.list], controllers.list)
+router.get('/:id', [auth()], controllers.show)
+router.patch('/:id', [auth({ onlyTheOwner: true }), validators.update], controllers.update)
+router.delete('/:id', [auth({ onlyTheOwner: true })], controllers.delete)
 
 module.exports = router
