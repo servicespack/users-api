88b7f514e0342e49a81208e2e1b5770399951bb6
Gabriel Rufino <contato@gabrielrufino.com>
2020-06-13 17:42:59 -0300

    Using in-memory mongodb for tests

--- index.js
+++ index.js
@@ -3,13 +3,20 @@
 const express = require('express')
 const path = require('path')
 
-require('./start')
+const { db } = require('./start')
 const app = require('./src/app')
-
-app.use('/', express.static(path.join(__dirname, '/docs')))
 
 const { APP_PORT } = process.env
 
-app.listen(APP_PORT, () => {
-  console.log('[app.js: Listening on ' + APP_PORT + ']')
+app.use('/', express.static(path.join(__dirname, '/docs')))
+
+const server = app.listen(APP_PORT, () => {
+  console.log(`[index.js: Listening on ${APP_PORT}]`)
 })
+
+module.exports = {
+  turnOff () {
+    db.close()
+    server.close()
+  }
+}
