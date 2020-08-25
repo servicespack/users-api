6c70e3ecce50dd093b6e7c6769891c224b86a053
Gabriel Rufino <contato@gabrielrufino.com>
2020-08-24 22:26:51 -0300

    New layer 'listeners' with initial listener UserListener

---
+++ src/listeners/UserListener.js
@@ -0,0 +1,19 @@
+const UserEmitter = require('../emitters/UserEmitter')
+const mailer = require('../helpers/mailer')
+
+const { MAILER_CONFIRM_URI } = process.env
+
+UserEmitter.on('create', ({ _id, email, emailVerificationKey }) => {
+  mailer
+    .sendMail(
+      email,
+      'Account Confirmation',
+      `
+        <div>
+          <a href="${MAILER_CONFIRM_URI}?key=${emailVerificationKey}&user_id=${_id}">Clique aqui</a> para confirmar seu E-mail
+        </div>
+      `
+    )
+})
+
+UserEmitter.on('delete', () => {})
