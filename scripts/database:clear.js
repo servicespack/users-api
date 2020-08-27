5034ba2323baa22ee52d69d4453c4743ae16c8c5
Gabriel Rufino <contato@gabrielrufino.com>
2020-08-26 21:10:39 -0300

    Loaing in database:clear

--- scripts/database:clear.js
+++ scripts/database:clear.js
@@ -1,16 +1,25 @@
 const { db } = require('../start/')
 
-if (process.env.APP_ENV === 'development') {
-  const mongoose = require('mongoose')
-
-  const User = mongoose.model('User')
-
-  User
-    .deleteMany({})
-    .then(async () => {
-      console.log('All users deleted')
-      db.close()
-    })
-} else {
+if (process.env.APP_ENV !== 'development') {
   throw new Error('Script only allowed in development')
 }
+
+const mongoose = require('mongoose')
+const ora = require('ora')
+
+const loading = ora('Cleaning database')
+loading.start()
+
+const User = mongoose.model('User')
+
+User
+  .deleteMany({})
+  .then(async () => {
+    loading.succeed('Database cleaned')
+  })
+  .catch(() => {
+    loading.fail('Error when cleaning database')
+  })
+  .finally(() => {
+    db.close()
+  })
