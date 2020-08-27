565b7613bc7e97898a6f82b6e6de7abc1f5a298c
Gabriel Rufino <contato@gabrielrufino.com>
2020-08-26 21:05:10 -0300

    Loading in database:populate

--- scripts/database:populate.js
+++ scripts/database:populate.js
@@ -1,27 +1,39 @@
 const { db } = require('../start/')
 
-if (process.env.APP_ENV === 'development') {
-  const bcrypt = require('bcryptjs')
+async function main () {
+  if (process.env.APP_ENV !== 'development') {
+    throw new Error('Script only allowed in development')
+  }
+
   const faker = require('faker')
   const mongoose = require('mongoose')
+  const ora = require('ora')
+  const { genSaltSync, hashSync } = require('bcryptjs')
 
-  const salt = bcrypt.genSaltSync(10)
-
-  const users = Array(100).fill(null).map(() => ({
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    username: faker.internet.userName(),
-    password: bcrypt.hashSync(faker.internet.password(), salt)
-  }))
+  const loading = ora('Populating the database')
+  loading.start()
 
   const User = mongoose.model('User')
+  const salt = await genSaltSync(10)
 
-  User
-    .insertMany(users)
-    .then(async () => {
-      console.log('Fake users inserted')
-      db.close()
-    })
-} else {
-  throw new Error('Script only allowed in development')
+  try {
+    for (let i = 1; i <= 100; i++) {
+      const user = {
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+        password: hashSync(faker.internet.password(), salt)
+      }
+
+      await User.create(user)
+    }
+
+    loading.succeed('Database populated')
+  } catch (error) {
+    loading.fail('Error when populating the database')
+  } finally {
+    db.close()
+  }
 }
+
+main()
