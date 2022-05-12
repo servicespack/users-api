30da233328ae11f92f7c85d6e04e9dc82eddda4a
Gabriel Rufino <contato@gabrielrufino.com>
2022-05-12 18:52:23 -0300

    Removing old docs

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 'use strict'
 
-const express = require('express')
-const path = require('path')
-
 const { cooldown, db } = require('./start')
 const app = require('./src/app')
 
 const { APP_PORT } = process.env
-
-app.use('/', express.static(path.join(__dirname, '/docs')))
 
 const server = app.listen(APP_PORT, () => {
   console.log(`[index.js: Listening on ${APP_PORT}]`)
