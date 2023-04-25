aedcf1bffade801d2f4cff2db9981824a5aea4ab
Gabriel Rufino <contato@gabrielrufino.com>
2023-04-25 08:25:25 -0300

    Using pino logger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import helmet from 'helmet'
+import pino from 'pino-http'
 
 import routers from './routers'
 
@@ -9,6 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(helmet())
+app.use(pino())
 
 app.use('/api', routers.root)
 app.use('/api/users', routers.users)
