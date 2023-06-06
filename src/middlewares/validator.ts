f7bff3e2d4751c5769a0c58dc3f991c14d739fde
Gabriel Rufino <contato@gabrielrufino.com>
2023-06-06 11:38:13 -0300

    Refactoring validator middleware

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -1,12 +1,12 @@
-import { plainToClass, type ClassConstructor } from 'class-transformer'
-import { validate } from 'class-validator'
+import { type ClassConstructor, plainToInstance } from 'class-transformer'
 import { type NextFunction, type Request, type Response } from 'express'
+import { validate } from 'class-validator'
 
 export const validator = (params: {
   Dto: ClassConstructor<unknown>
 }) => async (request: Request, response: Response, next: NextFunction) => {
   const payload = request.method === 'GET' ? request.query : request.body
-  const dto = plainToClass(params.Dto, payload)
+  const dto = plainToInstance(params.Dto, payload)
   const errors = await validate(dto as object)
 
   if (errors.length > 0) {
