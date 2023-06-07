fb26f0399636402cd5f7da99b57ef8015d67911e
Gabriel Rufino <contato@gabrielrufino.com>
2023-06-07 09:25:58 -0300

    Removing unused validators

--- src/middlewares/validators/users.ts
+++ src/middlewares/validators/users.ts
@@ -2,66 +2,6 @@ import { type NextFunction, type Request, type Response } from 'express'
 import validate from 'validate.js'
 
 export default {
-  create: async (request: Request, response: Response, next: NextFunction) => {
-    const constraints = {
-      name: {
-        presence: true,
-        length: {
-          minimum: 3
-        }
-      },
-      email: {
-        presence: true,
-        email: true
-      },
-      username: {
-        presence: true,
-        length: {
-          minimum: 3
-        }
-      },
-      password: {
-        presence: true,
-        length: {
-          minimum: 8
-        }
-      }
-    }
-
-    try {
-      await validate.async(request.body, constraints)
-    } catch (errors) {
-      console.log({ errors })
-      return response.status(400).json(errors)
-    }
-
-    next()
-  },
-  update: async (request: Request, response: Response, next: NextFunction) => {
-    const constraints = {
-      name: {
-        length: {
-          minimum: 3
-        }
-      },
-      email: {
-        email: true
-      },
-      username: {
-        length: {
-          minimum: 3
-        }
-      }
-    }
-
-    try {
-      await validate.async(request.body, constraints)
-    } catch (errors) {
-      return response.status(400).json(errors)
-    }
-
-    next()
-  },
   updatePassword: async (request: Request, response: Response, next: NextFunction) => {
     const constraints = {
       current_password: {
