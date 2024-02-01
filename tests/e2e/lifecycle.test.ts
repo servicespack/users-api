e1cb67fbd783b3cabbb34073c85dcd5a92809542
Gabriel Rufino <contato@gabrielrufino.com>
2024-01-31 22:02:20 -0300

    Lint fix

--- tests/e2e/lifecycle.test.ts
+++ tests/e2e/lifecycle.test.ts
@@ -1,13 +1,13 @@
-import { faker } from '@faker-js/faker'
-import jwt from 'jsonwebtoken'
-import supertest from 'supertest'
+import { faker } from '@faker-js/faker';
+import jwt from 'jsonwebtoken';
+import supertest from 'supertest';
 
-import { server } from '../../src/http.server'
-import { orm } from '../../src/start/database'
+import { server } from '../../src/http.server';
+import { orm } from '../../src/start/database';
 
 describe('Testing the users lifecycle', () => {
   // let app: any
-  let token: string
+  let token: string;
 
   beforeEach(() => {
     // process.env.APP_ENV = 'testing'
@@ -16,89 +16,87 @@ describe('Testing the users lifecycle', () => {
 
     // const { server } = await import('../../src/http.server')
     // app = server
-  })
+  });
 
   afterAll(async () => {
-    await orm.close()
-  })
+    await orm.close();
+  });
 
   const user = {
     name: faker.name.fullName(),
     email: faker.internet.email().toLowerCase(),
     username: faker.internet.userName().toLowerCase(),
-    password: faker.internet.password()
-  }
+    password: faker.internet.password(),
+  };
 
-  test('Should create an user', () => {
-    return supertest(server)
-      .post('/api/users')
-      .send(user)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(201)
-  })
+  test('Should create an user', () => supertest(server)
+    .post('/api/users')
+    .send(user)
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(201));
 
   test('Should create an token for authentication', async () => {
     const { body } = await supertest(server)
       .post('/api/tokens')
       .send({
         username: user.username,
-        password: user.password
+        password: user.password,
       })
       .set('Accept', 'application/json')
-      .expect(201)
+      .expect(201);
 
     expect(body).toEqual({
-      Authorization: expect.stringMatching(/^Bearer [A-Za-z0-9-._~+/]+=*(?:\.[A-Za-z0-9-._~+/]+=*)*$/)
-    })
+      Authorization: expect.stringMatching(/^Bearer [A-Za-z0-9-._~+/]+=*(?:\.[A-Za-z0-9-._~+/]+=*)*$/),
+    });
 
-    token = body.Authorization
-  })
+    token = body.Authorization;
+  });
 
   test('Should list a page of users', async () => {
     const { body } = await supertest(server)
       .get('/api/users')
       .set('Authorization', token)
-      .expect(200)
+      .expect(200);
 
     expect(body).toMatchObject({
       meta: {
         page: expect.any(Number),
         size: expect.any(Number),
-        total: expect.any(Number)
+        total: expect.any(Number),
       },
       data: [
         {
           id: expect.any(String),
           name: user.name,
           username: user.username.toLowerCase(),
-          email: user.email.toLowerCase()
-        }
-      ]
-    })
-  })
+          email: user.email.toLowerCase(),
+        },
+      ],
+    });
+  });
 
   test('Should detail the user', async () => {
-    const id = jwt.decode(token.split(' ')[1])?.sub
+    const id = jwt.decode(token.split(' ')[1])?.sub;
 
     const { body } = await supertest(server)
       .get(`/api/users/${id}`)
       .set('Authorization', token)
-      .expect(200)
+      .expect(200);
 
     expect(body).toMatchObject({
       name: user.name,
       email: user.email.toLowerCase(),
-      username: user.username.toLowerCase()
-    })
-  })
+      username: user.username.toLowerCase(),
+    });
+  });
 
   test('Should delete the user', () => {
-    const id = jwt.decode(token.split(' ')[1])?.sub
+    const id = jwt.decode(token.split(' ')[1])?.sub;
 
     return supertest(server)
       .delete(`/api/users/${id}`)
       .set('Authorization', token)
-      .expect(204)
-  })
-})
+      .expect(204);
+  });
+});
